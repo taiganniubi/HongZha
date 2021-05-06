0a49292df360a049d84cfd26dc2a112e753caca3
beichen123-hub <192201339@qq.com>
2021-05-06 22:15:16 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<!--	
+include './includes/common.php'; 
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (www.lxh5068.com/license)
+-->
+<html>
+	<head>
+   
+<title>小辉短信轰炸</title>
+<meta name="keywords" content="小辉资源论坛">
+<meta name="description" content="小辉资源论坛短信轰炸 ">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+					<h1 id="logo"><a href="/">主页</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="http://www.lxh5068.com/">小辉资源论坛</a></li>
+							<li><a href="http://BBS.lxh5068.com/">Blog</a>
+					        <li><a href="/user/reg.php">注册</a></li>
+							<li><a href="/user/login.php" target="_blank" class="button special">登录</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+						<header>
+							<h2>超强短信轰炸系统</h2>
+							<p>拥有几w+ 短信接口 急速响应！<br />
+							对各种骗子喷子说：“炸死你“”</p>
+						</header>
+						<span class="image"><img src="images/head_portrait.jpg" alt="轰炸" /></span>
+					</div>
+					<a href="#one" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- One -->
+				<section id="one" class="spotlight style1 bottom">
+					<span class="image fit main"><img src="images/pic001.jpg" alt="关于轰炸系统" /></span>
+					<div class="content">
+						<div class="container">
+							<div class="row">
+								<div class="4u 12u$(medium)">
+									<header>
+										<h2>关于小辉轰炸系统</h2>
+										<p>每天自动更新大量api接口</p>
+									</header>
+								</div>
+								<div class="4u 12u$(medium)">
+									<p>特点：
+									<br />高效高速 高质量轰炸
+									</p>
+								</div>
+								<div class="4u$ 12u$(medium)">
+									<p>惜梦技术论坛短信轰炸系统
+									<br />由小辉资源论坛提供 服务 支持！
+									<br />支持人脸识别登录
+									<br />API对接权限
+                                    <br />支持手动更新
+                                    </p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Two -->
+				<section id="two" class="spotlight style2 right">
+					<span class="image fit main"><img src="images/pic002.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>登录用户</h2>
+							<p>支持人脸识别</p>！
+						</header>
+						<p>
+						欢迎您的访问！
+						<br />超强轰炸开启中！
+						<ul class="actions">
+							<li><a href="/user/reg.php" class="button">进入</a></li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Three -->
+				<section id="three" class="spotlight style3 left">
+					<span class="image fit main bottom"><img src="images/pic003.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>注册</h2>
+							<p>注册一个账号，立送余额。</p>
+						</header>
+						<p>稳定测试	</p>
+						<ul class="actions">
+							<li><a href="/user/reg.php" target="_blank" class="button">注册一个账号</a></li>
+						</ul>
+					</div>
+					<a href="#four" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Four -->
+				<section id="four" class="wrapper style1 special fade-up">
+					<div class="container">
+						<header class="major">
+							<h2>网站设计</h2>
+							<p>独家UI设计！</p>
+						</header>
+						<div class="box alt">
+							<div class="row uniform">
+								<section class="4u 6u(medium) 12u$(xsmall)">
+									<span class="icon alt major fa-area-chart"></span>
+									<h3>在线API对接</h3>
+									<p>支持api对接</p>
+								</section>
+								<section class="4u 6u$(medium) 12u$(xsmall)">
+									<span class="icon alt major fa-comment"></span>
+									<h3>急速响应</h3>
+									<p>千万接口轻点一下急速响应</p>
+								</section>
+								<section class="4u$ 6u(medium) 12u$(xsmall)">
+									<span class="icon alt major fa-flask"></span>
+									<h3>高质量</h3>
+									<p>每轮攻击长达10多分钟 统计平均100条/分钟</p>
+								</section>
+					
+									
+					
+						</footer>
+					</div>
+				</section>
+
+			<!-- Five -->
+				<section id="five" class="wrapper style2 special fade">
+					<div class="container">
+						<header>
+							<h2>联系方式</h2>
+							<p>这里是我的联系方式呢.</p>
+						</header>
+						</div>
+		                        <p>小辉资源论坛：www.lxh5068.com</p>
+					<p>邮箱：3131282664@qq.com</p>
+				    <p>QQ群：97588668</p>
+                    <audio autoplay="autoplay"><source src="/mengzhan.mp3" type="audio/mpeg" /></audio>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<!--<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>-->
+						
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Night. All rights reserved.</li><li>Design: <a href="http://www.lxh5068.com/">小辉网络工作室</a></li>
+						
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?896657cfd762627291ee7bf931a8b164";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+	</html>
